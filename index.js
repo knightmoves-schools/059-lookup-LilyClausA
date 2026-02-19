dfddc50f31cfc6fbb2d3d23925b1834e7193d7db
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-02-19 14:21:32 -0600

    Add directions lookup object for N, S, E, W
    Create a directions object for cardinal directions.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+
+let directions = {
+  "N": "North",
+  "S": "South",
+  "E": "East",
+  "W": "West",
+};
+
+
+//should create a directions lookup object that looks up
+//the four abriviations N, S, E and W and returns their
+//corresponding full name like North
+
+
